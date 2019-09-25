95094ffb08227eac49fded728843e39b12a48a65
Serin Delaunay <serin.delaunay@gmail.com>
2019-09-25 18:24:02 +0100

    Reformat and make compatible with Python 3

--- mazegen.py
+++ mazegen.py
@@ -1,124 +1,147 @@
 #!/usr/bin/env python
 
-# Python < 3
+# Python 3
 # see LICENSE file for licensing information
 
 import random
 
-def getrandombit():	return random.randint(0, 1)
-def leftrandombit():	return getrandombit()
-def rightrandombit():	return getrandombit()
-def midrandombit():	return getrandombit()
-def generated(x):	pass
 
-def prrow(seed):
-	PF12 = ''
-	for i in range(8):
-		if seed & 1:
-			PF12 = 'XX' + PF12
-		else:
-			PF12 = '__' + PF12
-		seed >>= 1
-	PF012 = 'XXXX' + PF12
+def get_random_bit():
+    return random.randint(0, 1)
 
-	print PF012, PF012[::-1]
+
+def left_random_bit():
+    return get_random_bit()
+
+
+def right_random_bit():
+    return get_random_bit()
+
+
+def mid_random_bit():
+    return get_random_bit()
+
+
+def generated(x):
+    pass
+
+
+solid = 'XX'
+empty = '__'
+
+
+def pr_row(seed):
+    pf12 = ''
+    for i in range(8):
+        if seed & 1:
+            pf12 = solid + pf12
+        else:
+            pf12 = empty + pf12
+        seed >>= 1
+    pf012 = solid * 2 + pf12
+
+    print(pf012, pf012[::-1])
+
 
 # the mystery table from Entombed
 
 MAGIC = {
-	(0b00, 0b000):	1,
-	(0b00, 0b001):	1,
-	(0b00, 0b010):	1,
-	(0b00, 0b011):	None,		# None == random bit
-	(0b00, 0b100):	0,
-	(0b00, 0b101):	0,
-	(0b00, 0b110):	None,
-	(0b00, 0b111):	None,
+    (0b00, 0b000): 1,
+    (0b00, 0b001): 1,
+    (0b00, 0b010): 1,
+    (0b00, 0b011): None,  # None == random bit
+    (0b00, 0b100): 0,
+    (0b00, 0b101): 0,
+    (0b00, 0b110): None,
+    (0b00, 0b111): None,
 
-	(0b01, 0b000):	1,
-	(0b01, 0b001):	1,
-	(0b01, 0b010):	1,
-	(0b01, 0b011):	1,
-	(0b01, 0b100):	None,
-	(0b01, 0b101):	0,
-	(0b01, 0b110):	0,
-	(0b01, 0b111):	0,
+    (0b01, 0b000): 1,
+    (0b01, 0b001): 1,
+    (0b01, 0b010): 1,
+    (0b01, 0b011): 1,
+    (0b01, 0b100): None,
+    (0b01, 0b101): 0,
+    (0b01, 0b110): 0,
+    (0b01, 0b111): 0,
 
-	(0b10, 0b000):	1,
-	(0b10, 0b001):	1,
-	(0b10, 0b010):	1,
-	(0b10, 0b011):	None,
-	(0b10, 0b100):	0,
-	(0b10, 0b101):	0,
-	(0b10, 0b110):	0,
-	(0b10, 0b111):	0,
+    (0b10, 0b000): 1,
+    (0b10, 0b001): 1,
+    (0b10, 0b010): 1,
+    (0b10, 0b011): None,
+    (0b10, 0b100): 0,
+    (0b10, 0b101): 0,
+    (0b10, 0b110): 0,
+    (0b10, 0b111): 0,
 
-	(0b11, 0b000):	None,
-	(0b11, 0b001):	0,
-	(0b11, 0b010):	1,
-	(0b11, 0b011):	None,
-	(0b11, 0b100):	None,
-	(0b11, 0b101):	0,
-	(0b11, 0b110):	0,
-	(0b11, 0b111):	0,
+    (0b11, 0b000): None,
+    (0b11, 0b001): 0,
+    (0b11, 0b010): 1,
+    (0b11, 0b011): None,
+    (0b11, 0b100): None,
+    (0b11, 0b101): 0,
+    (0b11, 0b110): 0,
+    (0b11, 0b111): 0,
 }
 
-def rowgen(lastrows):
-	# prepend and append random bits to last row
-	lastrowpadded = leftrandombit()
-	lastrowpadded <<= 8
-	lastrowpadded |= lastrows[-1]
-	lastrowpadded <<= 1
-	lastrowpadded |= rightrandombit()
 
-	# last two bits generated in current row, initial value = 10
-	lasttwo = 0b10
+def row_gen(last_rows):
+    # prepend and append random bits to last row
+    last_row_padded = left_random_bit()
+    last_row_padded <<= 8
+    last_row_padded |= last_rows[-1]
+    last_row_padded <<= 1
+    last_row_padded |= right_random_bit()
 
-	newrow = 0
+    # last two bits generated in current row, initial value = 10
+    last_two = 0b10
 
-	# iterate from 7...0, inclusive
-	for i in range(7, -1, -1):
-		threeabove = (lastrowpadded >> i) & 0b111
+    new_row = 0
 
-		newbit = MAGIC[lasttwo, threeabove]
-		if newbit is None:
-			newbit = midrandombit()
-		newrow = (newrow << 1) | newbit
+    # iterate from 7...0, inclusive
+    for i in range(7, -1, -1):
+        three_above = (last_row_padded >> i) & 0b111
 
-		lasttwo = ( (lasttwo << 1) | newbit ) & 0b11
+        new_bit = MAGIC[last_two, three_above]
+        if new_bit is None:
+            new_bit = mid_random_bit()
+        new_row = (new_row << 1) | new_bit
 
-	# hook for verification
-	generated(newrow)
+        last_two = ((last_two << 1) | new_bit) & 0b11
 
-	# now do postprocessing
-	lastrows.append(newrow)
-	lastrows = lastrows[-11:]
+    # hook for verification
+    generated(new_row)
 
-	# condition 1
-	history = [ b & 0xf0 for b in lastrows ]
-	if 0 not in history:
-		if sum( [ b & 0x80 for b in history ] ) == 0:
-			#print 'pp 1'
-			lastrows[-1] = 0
+    # now do postprocessing
+    last_rows.append(new_row)
+    last_rows = last_rows[-11:]
 
-	# condition 2
-	history = [ b & 0xf for b in lastrows[-7:] ]
-	if 0 not in history:
-		comparator = 0
-		if len(lastrows) >= 9:
-			comparator = lastrows[-9]
-		if sum( [ b & 1 for b in history ] ) == (comparator & 1)*7:
-			#print 'pp 2'
-			lastrows[-1] &= 0xf0
+    # condition 1
+    history = [b & 0xf0 for b in last_rows]
+    if 0 not in history:
+        if sum([b & 0x80 for b in history]) == 0:
+            # print 'pp 1'
+            last_rows[-1] = 0
 
-	prrow(lastrows[-1])
-	return lastrows
-	
-def mazegen():
-	lastrows = [ 0 ]
-	while True:
-		lastrows = rowgen(lastrows)
+    # condition 2
+    history = [b & 0xf for b in last_rows[-7:]]
+    if 0 not in history:
+        comparator = 0
+        if len(last_rows) >= 9:
+            comparator = last_rows[-9]
+        if sum([b & 1 for b in history]) == (comparator & 1) * 7:
+            # print 'pp 2'
+            last_rows[-1] &= 0xf0
+
+    pr_row(last_rows[-1])
+    return last_rows
+
+
+def maze_gen():
+    last_rows = [0]
+    while True:
+        last_rows = row_gen(last_rows)
+
 
 if __name__ == '__main__':
-	#random.seed(12345)
-	mazegen()
+    # random.seed(12345)
+    maze_gen()
